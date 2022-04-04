7bda377e771273ce8e269b45f18614e0af0a7946
Alex <Alex@192.168.0.11>
2022-04-04 10:24:58 +0300

    Create PatternTask, divide tests to every classes

---
+++ ProcessingPatterns/Tests/ua/engexercises/ProcessingPatternTest.java
@@ -0,0 +1,37 @@
+package ua.engexercises;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ua.engexercises.model.CommonElements;
+import ua.engexercises.model.DBGetData;
+import ua.engexercises.model.PatternTask;
+import ua.engexercises.model.ProcessingExpressions;
+import ua.engexercises.model.ProcessingPattern;
+import ua.engexercises.model.RandomNumber;
+
+class TestRandomNumber extends RandomNumber {
+	public int getRandomNumber( int maxNumber ) {
+		return 0;
+	}
+}
+
+public class ProcessingPatternTest {
+	ProcessingPatternsTestData testData = new ProcessingPatternsTestData();
+	
+	@Test
+	void testProcessingPatternGetTask() throws Exception {
+		PatternTask patternData = new ProcessingPattern.Builder()
+				.setAnswer(testData.patternAnswerSeeSimple)
+				.setTask(testData.patternTaskSeeSimple)
+				.setElements(testData.commonElements)
+				.setRandomNumbers(testData.randomNumber)
+				.build()
+				.getTaskAndAnswer();
+		assertEquals( "see I/you", patternData.getTask() );
+		assertEquals( "I see you.", patternData.getAnswer() );
+	}
+}
